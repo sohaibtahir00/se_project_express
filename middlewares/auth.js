5c4d7b6ce16f3e816a6217b260314b5e3ede3c34
Sohaib Tahir <sohaibtahir90@gmail.com>
2024-11-06 15:47:34 -0600

    Project 15 v4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,18 +6,18 @@ const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new UnauthorizedError("Authorization required");
+    return next(new UnauthorizedError("Authorization required"));
   }
 
   const token = authHeader.split(" ")[1];
 
   return jwt.verify(token, JWT_SECRET, (err, payload) => {
     if (err) {
-      throw new UnauthorizedError("Invalid token");
+      return next(new UnauthorizedError("Invalid token"));
     }
 
     req.user = payload;
-    return next();
+    next();
   });
 };
 
